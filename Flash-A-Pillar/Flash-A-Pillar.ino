6ce5268f726220f18d705d62d9b6f66c20bf6316
Franck Fleurey <franck@fleurey.com>
2017-01-12 15:33:07 +0100

    Added some explainations

--- Flash-A-Pillar/Flash-A-Pillar.ino
+++ Flash-A-Pillar/Flash-A-Pillar.ino
@@ -22,95 +22,15 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 **/
 
-
 /*
  * Implementation of the Code-A-Pillar "master" protocol
- * - Data but to arduino pin 2
+ * - Data bus to arduino pin 2
  * - Enable of the firt block to arduino pin 3
  * 
  * This sketch will discover the tail periodically and flash 
  * the blocks.
  * 
  */
-
-
-void bus_high() {
-  DDRD &= ~_BV(PD2);   // set PD2 as input (Arduino pin 2)
-  PORTD |= _BV(PD2);  // pull-up
-}
-
-void bus_low() {
-  PORTD &= ~_BV(PD2);  // LOW
-  DDRD |= _BV(PD2);   // set PD2 as output (Arduino pin 2)
-  
-}
-
-uint8_t read_bus() {
-  if (PIND & _BV(PD2)) return 1;
-  else return 0;
-}
-
-void reset_bus() {
-  bus_low();
-  delayMicroseconds(800);
-  bus_high();
-}
-
-void send_bit_1() {
-  bus_low();
-  delayMicroseconds(10);
-  bus_high();
-  delayMicroseconds(90);
-}
-
-void send_bit_0() {
-  bus_low();
-  delayMicroseconds(60);
-  bus_high();
-  delayMicroseconds(40);
-}
-
-uint8_t read_bit() {
-  bus_low();
-  delayMicroseconds(10);
-  bus_high();
-  delayMicroseconds(30);
-  uint8_t result = read_bus();
-  delayMicroseconds(60);
-  return result;
-}
-
-void send_bits(uint8_t b, uint8_t nb_bits) {
-  int8_t i;
-  for(i=nb_bits-1; i>=0; i--) {
-    if (b & (1<<i)) send_bit_1();
-    else send_bit_0();
-  }
-}
-
-uint8_t read_bits(uint8_t nb_bits) {
-  int8_t i;
-  uint8_t result = 0;
-  for(i=nb_bits-1; i>=0; i--) {
-    if (read_bit()) result |= 1<<i;
-  }
-  return result;
-}
-
-void send_message(uint8_t a, uint8_t c, uint8_t p) {
-  send_bits(a, 8);
-  send_bits(c, 8);
-  send_bits(p, 8);
-  send_bits(a+c+p, 4); // Send Checksum  
-}
-
-uint16_t read_response() {
-  uint8_t b1 = read_bits(8);
-  uint8_t b2 = read_bits(8);
-  uint8_t sum = read_bits(4);
-  if (sum == ((b1+b2) & 0x0F)) return (b1<<8) + b2;
-  else return 0xFFFF; // Checksum Error
-}
 
 uint16_t send_discover(uint8_t addr) {
   send_message(addr, 0x01, 0);
@@ -197,10 +117,89 @@ void loop() {
     send_led_off(i);
     delay(10);
   }
-
   delay(2000);
 
 }
 
+/********* LOW LEVEL IMPLEMENTATION OF THE 1-WIRE LIKE PROTOCOL **********/
 
+// NOTE: The internal pullup of the uC is used to pull the line high. It works but it is a bit weak.
+// A 2.2k resistor can be added to the circuit to pull the data line to VCC.
+void bus_high() {
+  DDRD &= ~_BV(PD2);   // set PD2 as input (Arduino pin 2)
+  PORTD |= _BV(PD2);  // pull-up 
+}
+
+void bus_low() {
+  PORTD &= ~_BV(PD2);  // LOW
+  DDRD |= _BV(PD2);   // set PD2 as output (Arduino pin 2)
+  
+}
+
+uint8_t read_bus() {
+  if (PIND & _BV(PD2)) return 1;
+  else return 0;
+}
+
+void reset_bus() {
+  bus_low();
+  delayMicroseconds(800);
+  bus_high();
+}
+
+void send_bit_1() {
+  bus_low();
+  delayMicroseconds(10);
+  bus_high();
+  delayMicroseconds(90);
+}
+
+void send_bit_0() {
+  bus_low();
+  delayMicroseconds(60);
+  bus_high();
+  delayMicroseconds(40);
+}
+
+uint8_t read_bit() {
+  bus_low();
+  delayMicroseconds(10);
+  bus_high();
+  delayMicroseconds(30);
+  uint8_t result = read_bus();
+  delayMicroseconds(60);
+  return result;
+}
+
+void send_bits(uint8_t b, uint8_t nb_bits) {
+  int8_t i;
+  for(i=nb_bits-1; i>=0; i--) {
+    if (b & (1<<i)) send_bit_1();
+    else send_bit_0();
+  }
+}
+
+uint8_t read_bits(uint8_t nb_bits) {
+  int8_t i;
+  uint8_t result = 0;
+  for(i=nb_bits-1; i>=0; i--) {
+    if (read_bit()) result |= 1<<i;
+  }
+  return result;
+}
+
+void send_message(uint8_t a, uint8_t c, uint8_t p) {
+  send_bits(a, 8);
+  send_bits(c, 8);
+  send_bits(p, 8);
+  send_bits(a+c+p, 4); // Send Checksum  
+}
+
+uint16_t read_response() {
+  uint8_t b1 = read_bits(8);
+  uint8_t b2 = read_bits(8);
+  uint8_t sum = read_bits(4);
+  if (sum == ((b1+b2) & 0x0F)) return (b1<<8) + b2;
+  else return 0xFFFF; // Checksum Error
+}
 
--- Flash-A-Pillar/README.md
+++ Flash-A-Pillar/README.md
@@ -1,8 +1,34 @@
-# Flash A Pillar
+# Flash-A-Pillar
 
-This sketch implements the code a pillar protocol to communicate with the Code-a-Pillar tail.
+This sketch implements protocol to communicate with the Code-a-Pillar tail. The head/body of the code a pillar is replaced by an Arduino which can discover the tail and send commands to the different modules.
 
-Connections: Data bus to arduino pin 2 and "enable" on arduino pin 3.
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=aNHJMacFLYw" target="_blank"><img src="http://img.youtube.com/vi/aNHJMacFLYw/0.jpg" 
+alt="youtube video" width="320" height="240" border="10" /></a>
+
+## Protocol
+
+When first connected, the tail blocks are waiting for a reset to happen on the bus in order to initialize. Upon reset, the modules are initialized in the order they are connected on the bus and get their address on the bus. Addresses start at 1 for the first block which is connected to the head (or Arduino is our case). The order in which blocks are initialized is enforced by the "enable" signals. Upon reset all blocks set their enable signals high. The head keeps it low. After the reset, only one block has a low "enable": the one connected to the head. After it is initialized, it sets its "enable" line low in order to allow the second block to initialize. It works the same for the rest of the tail.
+
+During the initialisation, the head send "discover" initialization messages starting with address 1 and increments the address as long as it gets replies from the tail. In the reply to the initialization messages, the tail blocks indicate their types and implicitly acknowledge their address. After the initialization, the master thus has fully discovered the order and types of the blocks that makes up the tail.
+
+Once the blocks are initialized, there are 5 main commands the head can send to individual blocks (there are also a few broadcast commands but they are not yet implemented in the sketch):
+ 
+ * **DISCOVER**: That is the commend used for the initialization. In normal use, the head is periodically sending discover messages with the first non allocated address (address of the last block + 1) in order to detect if any new blocks are connected to the tail.
+ * **PING**: Ping the address of one block. The block acknowledges the packet to indicate that it is still present and running. In the normal use of the code-a-pillar, the head is constantly pinging the tail blocks in order to detect if and block have been disconnected.
+ * **LED_ON**: Turn the LED of the block ON.
+ * **LED_OFF**: Turn the LED of the block OFF.
+ * **LED_BLINK**: Blink the LED of the block.
+
+All commands are acknowledged by the blocks.
+
+## Hardware
+
+* Arduino Uno (or any other arduino flavour)
+* A USB female connector (For example from a USB extension cable)
+
+Code-a-pillar data bus is connected to arduino pin 2 and "enable" on arduino pin 3 (or connected to ground).
+
+## Software
 
 The sketch periodically resets the bus and discovers the tail in the order of the blocks. It then flashes the blocks and blink them.
 
